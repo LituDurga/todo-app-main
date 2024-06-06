cb22bdbe2e47591fc438bda0b2f0c97518ac169d
Durga Prasad Padhy <dppadhy101@gmail.com>
2024-06-06 23:54:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-sm navbar-light bg-primary">
+        <a className="navbar-brand mx-3" href="#">
+          Durga
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+
+
+      <AddTodo />
+      <Todos />
+
+    </>
+  );
+}
+
+export default App;
